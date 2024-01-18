8733ed8006b07cafeba835725c406b79511a494b
Vladimir Andrusevich <vl.andrusevich@yandex.by>
2024-01-18 13:40:20 +0300

    Fix bugs with H2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO "genres" ("name")
+INSERT INTO genres (name)
 VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
-INSERT INTO "ratings" ("name")
+INSERT INTO ratings (name)
 VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
